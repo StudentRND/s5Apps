deffadd55c7bae7d8e3158a338e1097713709391
Tyler Menezes <tylermenezes@gmail.com>
2014-03-05 19:13:19 -0800

    Remove port

--- include/s5/API.php
+++ include/s5/API.php
@@ -121,10 +121,8 @@ class API
 
         $sp = strtolower($s['SERVER_PROTOCOL']);
         $protocol = substr($sp, 0, strpos($sp, '/')) . (($ssl) ? 's' : '');
-        $port = $s['SERVER_PORT'];
-        $port = ((!$ssl && $port=='80') || ($ssl && $port=='443')) ? '' : ':'.$port;
         $host = isset($s['HTTP_X_FORWARDED_HOST']) ? $s['HTTP_X_FORWARDED_HOST'] : isset($s['HTTP_HOST']) ? $s['HTTP_HOST'] : $s['SERVER_NAME'];
-        return $protocol . '://' . $host . $port . $s['REQUEST_URI'];
+        return $protocol . '://' . $host . $s['REQUEST_URI'];
     }
 
     public function GET($endpoint, $params = [])
